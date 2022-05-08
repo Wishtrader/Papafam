39cf7627dd30e50c97090ae9d83ee8937d885733
Andrei Kamarou <mr.akomarov@gmail.com>
2022-05-09 01:43:16 +0300

    Initial commit

---
+++ components/Sidebar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Sidebar() {
+	return (
+		<Container>
+			Sidebar
+		</Container>
+	)
+}
+
+export default Sidebar
+
+const Container = styled.div`
+
+`
---
+++ pages/chat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function chat() {
+	return (
+		<div>Chat Page</div>
+	)
+}
+
+export default chat
